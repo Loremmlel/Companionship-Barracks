fbaeaf15a74ea2be6c1f047fb1f9f4205fdd9fb4
柚子 <loremmlel@163.com>
2025-12-22 12:02:59 +0800

    Enhance MemoryThoughtHandler patch to modify memory thought based on sibling relationships

--- MemoryThoughtHandler_Patch.cs
+++ MemoryThoughtHandler_Patch.cs
@@ -1,19 +1,41 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System.Linq;
 using Verse;
 
 namespace Companionship_Barracks
 {
-	[HarmonyPatch(typeof(MemoryThoughtHandler), nameof(MemoryThoughtHandler.TryGainMemory), new[] { typeof(Thought_Memory), typeof(Pawn) })]
-	public static class MemoryThoughtHandler_Patch
-	{
-		public static bool Prefix(MemoryThoughtHandler __instance, Thought_Memory newThought, Pawn otherPawn)
-		{
-			if (newThought.def != ThoughtDefOf.GotSomeLovin || otherPawn == null)
-			{
-				return true;
-			}
-			var pawn = __instance.pawn;
-		}
-	}
+    [HarmonyPatch(typeof(MemoryThoughtHandler), nameof(MemoryThoughtHandler.TryGainMemory), typeof(Thought_Memory),
+        typeof(Pawn))]
+    public static class MemoryThoughtHandler_Patch
+    {
+        public static bool Prefix(MemoryThoughtHandler __instance, Thought_Memory newThought, Pawn otherPawn)
+        {
+            if (newThought.def != ThoughtDefOf.GotSomeLovin || otherPawn == null)
+            {
+                return true;
+            }
+
+            var pawn = __instance.pawn; // 获取当前Pawn
+            if (pawn == null)
+            {
+                return true;
+            }
+
+            var  relations        = pawn.GetRelations(otherPawn);
+            bool isSibling        = relations.Any(r => r.defName == PawnRelationDefOf.Sibling.defName);
+            bool isOppositeGender = pawn.gender                        != otherPawn.gender;
+            bool isOlder          = pawn.ageTracker.AgeBiologicalYears > otherPawn.ageTracker.AgeBiologicalYears;
+            bool isBrotherWithYoungerSister = isSibling && isOppositeGender &&
+                                              (pawn.gender == Gender.Male   && isOlder ||
+                                               pawn.gender == Gender.Female && !isOlder);
+            if (!isBrotherWithYoungerSister)
+            {
+                return true;
+            }
+
+            newThought.def = isOlder ? MakeLoveDefOf.MakeLoveWithSister : MakeLoveDefOf.MadeLoveWithBrother;
+            return true;
+        }
+    }
 }
