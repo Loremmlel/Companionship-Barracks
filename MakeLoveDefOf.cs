1b8f7939e94cf7f66d21b19d50463e969a7a1174
柚子 <loremmlel@163.com>
2025-12-22 12:02:46 +0800

    添加 MakeLoveDefOf 类及相关文件，更新项目文件以包含新定义

---
+++ MakeLoveDefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+
+namespace Companionship_Barracks
+{
+    [DefOf]
+    public static class MakeLoveDefOf
+    {
+        public static ThoughtDef MakeLoveWithSister;
+        public static ThoughtDef MadeLoveWithBrother;
+        
+        static MakeLoveDefOf()
+        {   
+            DefOfHelper.EnsureInitializedInCtor(typeof(MakeLoveDefOf));
+        }
+    }
+}
